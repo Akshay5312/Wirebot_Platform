7f2e88ce3138f8b626341a1f0ee562ed69feb717
Akshay5312 <40531389+Akshay5312@users.noreply.github.com>
2023-01-16 13:25:39 -0500

    creating a better wirebot class, trajectory following at 50 ms intervals

--- Wirebot.h
+++ Wirebot.h
@@ -1,16 +1,99 @@
 #include "SpaceTransforms/WirebotTransformation.h"
 
-struct Actuators{
-    float values[3] = {0};
-    float speeds[3] = {0};
+
+/**
+ * @brief A representation of velocity controlled actuators
+ * 
+ * @tparam n number of actuators
+ */
+template <int n>
+class Actuators{
+    BLA::Matrix<n> _pos;
+    BLA::Matrix<n> _vel;
+    
+    public:
+
+    Actuators(BLA::Matrix<n> pos = {0,0,0}) : _pos(pos){
+        _vel = BLA::Zeros<n>();
+    }
+
+    operator BLA::Matrix<n>& () {
+        return _pos;
+    }
+
+    void setVel(BLA::Matrix<n> vel){_vel = vel;}
+
+    BLA::Matrix<n> step(float dt){
+        return (_pos = _pos + _vel * dt); 
+    }
 };
 
-class Wirebot : LLAMA::SpaceTransformations::WirebotTransformations{
+/**
+ * @brief A class representing the wirebot
+ * 
+ */
+class Wirebot : public LLAMA::SpaceTransformations::WirebotTransformations{
     private:
-        Actuators _actuators;
+        //the actuators
+        Actuators<3> _actuators;
 
+        //the position
+        BLA::Matrix<3> _p;
+        //the target position
+        BLA::Matrix<3> _targetp;
+        //the target speed
+        BLA::Matrix<3> _targetdp;
+
+        
+        BLA::Matrix<3> updatePosition(int accuracyHeuristic = 25){
+            _p = FK(_actuators, _p, accuracyHeuristic);
+            return _p;
+        }
+        
     public:
-        Wirebot(BLA::Matrix<3> a1, BLA::Matrix<3> a2, BLA::Matrix<3> a3) : LLAMA::SpaceTransformations::WirebotTransformations(a1, a2, a3){
+        Wirebot(BLA::Matrix<3> a1, BLA::Matrix<3> a2, BLA::Matrix<3> a3, BLA::Matrix<3> initConfig = {0,0,0}) : LLAMA::SpaceTransformations::WirebotTransformations(a1, a2, a3){
+            _p = initConfig;
 
+            _actuators = IK(_p);
+        }
+
+        Wirebot& run(bool runposition, float dt = 0.01){
+            updatePosition();
+            BLA::Matrix<3> setdp;
+            if(runposition){
+                setdp = (_targetp - _p) * 10;
+            }else{
+                setdp = _targetdp;
+            }
+
+            BLA::Matrix<3,3> currIJ = InverseJacobian(_p);
+
+            BLA::Matrix<3> setdq = currIJ * setdp;
+
+            _actuators.setVel(setdq);
+
+            _actuators.step(dt);
+
+            return (*this);
+        }
+
+
+        /**
+         * @brief cast this to a vector by returning it's position
+         * 
+         * @return BLA::Matrix<3> the position of the end effector
+         */
+        operator BLA::Matrix<3>(){return _p;}
+
+        /**
+         * @brief assign the target position
+         * 
+         * @param target 
+         * @return Wirebot& 
+         */
+        Wirebot& operator = (const BLA::Matrix<3> &target){ _targetp = target; return (*this);}
+
+        void setVel(BLA::Matrix<3> Vel){
+            _targetdp = Vel;
         }
 };
--- main.cpp
+++ main.cpp
@@ -7,15 +7,12 @@
 using namespace std;
 
 BLA::Matrix<3> circleTraj(BLA::Matrix<1> t){
-    return {sin(3*t(0)), 2*cos(3*t(0)), 3*sin(t(0)/10) + 5};
+    return {sin(3*t(0)), 2*cos(3*t(0)), 3*sin(t(0) * 12) + 5};
 }
 
 int main(){
     Wirebot Bot({-5.774,0,10}, {2.887,5,10}, {-2.887,-5,10});
 
-    BLA::Matrix<3> c =  Bot.getTraverser({10,10,10});
-
-    BLA::Matrix<3> q = Bot.getKinematics()->IK(c);
     float t = 0;
 
     ofstream myfile;
@@ -23,18 +20,10 @@ int main(){
     myfile.open("log.csv");
 
     while(t < 10){
-        BLA::Matrix<3> setpoint = circleTraj({t});
-
-        BLA::Matrix<3> disp = Bot.getKinematics()->InverseJacobian(c) * (setpoint - c);
-
-        q = q + disp * 0.3;
-        
-        c =  Bot.getTraverser(q); 
-
-        t = t + 0.1;
-
-        myfile<< c(0)<< ", "<<c(1)<<", "<<c(2) <<", "<< t<<'\n';
-
+        Bot = circleTraj({t});
+        BLA::Matrix<3> p = Bot.run(0.05);
+        myfile<< p(0)<< ", "<<p(1)<<", "<<p(2) <<", "<< t<<'\n';
+        t+=0.05;
     }
     myfile.close();
 
